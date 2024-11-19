c3a594da0c9ddff8371db6a04d36fe86f03bbdfb
zwarn <michael.budahn@gmail.com>
2024-11-19 23:37:18 +0100

    change terrain via tools in game

---
+++ Assets/Scripts/scriptableObjects/tool/ChangeTerrain.cs
@@ -0,0 +1,19 @@
+﻿using influence;
+using UnityEngine;
+using Zenject;
+
+namespace scriptableObjects.tool
+{
+    [CreateAssetMenu(fileName = "AddInfluence", menuName = "tools/AddInfluence", order = 0)]
+    public class ChangeTerrain : SelectableTool
+    {
+        public int amount;
+
+        [Inject] private InfluenceController _influenceController;
+
+        public override void Apply(int x, int y)
+        {
+            _influenceController.AddInfluence(x, y, amount);
+        }
+    }
+}
