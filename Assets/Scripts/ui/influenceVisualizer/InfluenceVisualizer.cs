c72e045993df7e33097c5658ef0c031581b56e40
zwarn <michael.budahn@gmail.com>
2024-11-21 21:11:00 +0100

    show influence values around the mouse position

---
+++ Assets/Scripts/ui/influenceVisualizer/InfluenceVisualizer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using influence;
+using map;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace ui.influenceVisualizer
+{
+    public class InfluenceVisualizer : MonoBehaviour
+    {
+        private const int GridSize = 3;
+
+        [SerializeField] private InfluenceVisualizerTileView prefab;
+
+        private Dictionary<Vector2Int, InfluenceVisualizerTileView> _tileViews = new();
+
+        [Inject] private MapController _mapController;
+        [Inject] private InfluenceController _influenceController;
+
+        private void Start()
+        {
+            for (int x = -GridSize + 1; x < GridSize; x++)
+            {
+                for (int y = -GridSize + 1; y < GridSize; y++)
+                {
+                    Vector2Int relativPos = new Vector2Int(x, y);
+                    var tileView = Instantiate(prefab, transform);
+                    tileView.transform.localPosition = new Vector3(x, y);
+                    _tileViews.Add(relativPos, tileView);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            var worldMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var x = (int)worldMouse.x;
+            var y = (int)worldMouse.y;
+
+            if (_mapController.IsPointOnMap(x, y))
+            {
+                transform.position = new Vector3(x, y);
+            }
+
+            UpdateTileViews(x, y);
+        }
+
+        private void UpdateTileViews(int centerX, int centerY)
+        {
+            for (int x = -GridSize + 1; x < GridSize; x++)
+            {
+                for (int y = -GridSize + 1; y < GridSize; y++)
+                {
+                    int worldX = centerX + x;
+                    int worldY = centerY + y;
+                    var tileView = _tileViews[new Vector2Int(x, y)];
+
+                    if (_mapController.IsPointOnMap(worldX, worldY))
+                    {
+                        tileView.SetValue(_influenceController.GetGrid().GetValue(worldX, worldY));
+                        tileView.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        tileView.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ui/influenceVisualizer/InfluenceVisualizerTileView.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ui.influenceVisualizer
+{
+    public class InfluenceVisualizerTileView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text text;
+
+        public void SetValue(double value)
+        {
+            text.text = value.ToString("F");
+        }
+    }
+}
